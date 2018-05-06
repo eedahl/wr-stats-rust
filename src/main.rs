74b5e5e9f2e22a2b0c7537e8d536734cbb2bd91b
eedahl <eivind.dahl@gmail.com>
2018-05-06 03:18:01 +0200

    Fixed diff and refactored a bit

--- src/main.rs
+++ src/main.rs
@@ -44,9 +44,9 @@ fn time_from_string(st : &String) -> Time {
 }
 
 fn time_difference(t1 : &Time, t2 : &Time) -> Time {
-    let mut m = t1.minutes - t2.minutes;
-    let mut s = 0;
-    let mut ms = 0;
+    let mut ms = t1.milliseconds - t2.milliseconds;
+    let mut s;
+    let m;
 
     if ms < 0 {
         s = t1.seconds - t2.seconds - 1;
@@ -66,9 +66,9 @@ fn time_difference(t1 : &Time, t2 : &Time) -> Time {
 }
 
 fn add_times(t1 : Time, t2 : Time) -> Time{
-    let mut m = t1.minutes + t2.minutes;
-    let mut s = 0;
-    let mut ms = 0;
+    let mut ms = t1.milliseconds + t2.milliseconds;
+    let mut s;
+    let m;
 
     if ms > 99 {
         s = t1.seconds + t2.seconds + 1;
@@ -94,7 +94,7 @@ fn test_time_to_string() {
  }
 
 fn main() {
-    let level_names = [
+    let level_names = vec![
         "Warm Up",
         "Flat Track",
         "Twin Peaks",
@@ -159,56 +159,45 @@ fn main() {
     // Read WR table data
     let mut wr_tables = Vec::new();
 
-    {
-        let mut r = csv::Reader::from_file("2018-04-19_elma_wrs.csv").unwrap();
+    let mut r = csv::Reader::from_file("2018-04-19_elma_wrs.csv").unwrap();
 
-        for record in r.records() {
-            let row = record.unwrap();
-            wr_tables.push(WR{ 
-                table: row[0].parse::<i32>().unwrap(),
-                lev : row[1].parse::<i32>().unwrap(),
-                time : time_from_string(&row[3].to_string()),
-                kuski : row[4].to_string() });
-        }
+    for record in r.records() {
+        let row = record.unwrap();
+        wr_tables.push(WR{ 
+            table: row[0].parse::<i32>().unwrap(),
+            lev : row[1].parse::<i32>().unwrap(),
+            time : time_from_string(&row[3].to_string()),
+            kuski : row[4].to_string() });
     }
 
     // Read PR data
     let mut time_table = Vec::new();
 
-    {
-        let mut f = std::fs::File::open("stats.txt").expect("Cannot open file: stats.txt");
-        let mut c = String::new();
-        f.read_to_string(&mut c).expect("Cannot read file: stats.txt");
-        
-        let mut level_counter = 0;
-        let mut level_found = false;
-        for line in c.lines() {
-            let mut data : Vec<&str> = line.trim().split_whitespace().collect();
+    let mut f = std::fs::File::open("stats.txt").expect("Cannot open file: stats.txt");
+    let mut c = String::new();
+    f.read_to_string(&mut c).expect("Cannot read file: stats.txt");
+    
+    let mut level_counter = 0;
+    let mut level_found = false;
+    for line in c.lines() {
+        let mut data : Vec<&str> = line.trim().split_whitespace().collect();
 
-            if data.len() != 0 && level_found {
-                time_table.push(time_from_string(&String::from(data[0])));
-                level_counter += 1;
-                level_found = false;
-            }
+        if data.len() != 0 && level_found {
+            time_table.push(time_from_string(&String::from(data[0])));
+            level_counter += 1;
+            level_found = false;
+        }
 
-            if data.len() != 0 && data[0] == "Level" {
-                level_found = true;
-            }
+        if data.len() != 0 && data[0] == "Level" {
+            level_found = true;
+        }
 
-            if level_counter == 54 {
-                break;
-            }
+        if level_counter == 54 {
+            break;
         }
     }
 
     let headers = vec!["Lev", "Name", "PR", "Table", "Time", "Kuski", "Target", "Diff", "Kuski"];
-
-    let mut last_table_beat : String;
-    let mut last_time_beat : String;
-    let mut last_kuski_beat : String;
-    let mut next_target : String;
-    let mut diff : String;
-    let mut next_kuski : String;
 
     let mut data = String::new();
 
@@ -217,34 +206,34 @@ fn main() {
     for i in 0..54 {
         let t = &time_table[i];
         let lev : i32 = (i as i32) + 1;
-        let last_wr_beat = &wr_tables.iter().filter(|x| (x.lev == lev) && compare_times(t, &x.time)).last();
-        let first_wr_not_beat = &wr_tables.iter().filter(|x| (x.lev == lev) && !compare_times(t, &x.time)).nth(0);
+        let last_wr_beat = wr_tables.iter().filter(|x| (x.lev == lev) && compare_times(t, &x.time)).last();
+        let first_wr_not_beat = wr_tables.iter().filter(|x| (x.lev == lev) && !compare_times(t, &x.time)).nth(0);
 
         let lev_number = lev.to_string();
         let lev_name = level_names[i];
         let pr = time_to_string(t);
     
-        if last_wr_beat.is_some() {
-            last_table_beat = last_wr_beat.unwrap().table.to_string();
-            last_time_beat = time_to_string(&last_wr_beat.unwrap().time);
-            last_kuski_beat = last_wr_beat.unwrap().kuski.clone();
+        let (last_table_beat, last_time_beat, last_kuski_beat) = if let Some(wr) = last_wr_beat {
+            (wr.table.to_string(),
+            time_to_string(&wr.time),
+            wr.kuski.clone())
         }
         else {
-            last_table_beat = String::from("-");
-            last_time_beat = String::from("-");
-            last_kuski_beat = String::from("-");
-        }
+            (String::from("-"),
+            String::from("-"),
+            String::from("-"))
+        };
 
-        if first_wr_not_beat.is_some() {
-            next_target = time_to_string(&first_wr_not_beat.unwrap().time);
-            diff = "+".to_owned() + &time_to_string(&time_difference(t, &first_wr_not_beat.unwrap().time));
-            next_kuski = first_wr_not_beat.unwrap().kuski.clone();
+        let (next_target, diff, next_kuski) = if let Some(wr) = first_wr_not_beat {
+            (time_to_string(&wr.time),
+            "+".to_owned() + &time_to_string(&time_difference(t, &wr.time)),
+            wr.kuski.clone())
         }
         else {
-            next_target = String::from("-");
-            diff = String::from("-");
-            next_kuski = String::from("-");
-        }
+            (String::from("-"),
+            String::from("-"),
+            String::from("-"))
+        };
         
         data.push_str(&format!("{:<5}{:<19}{:<10}{:<7}{:<10}{:<13}{:<10}{:<11}{:<13}\r\n",
             lev_number,
@@ -258,10 +247,9 @@ fn main() {
             next_kuski));
     }
 
-    {
-        let mut f = std::fs::File::create("wrs_beat.txt").expect("Could not create file: wrs_beat.txt");
-        f.write_all(&data.into_bytes()).expect("Could not write to file: wrs_beat.txt");
-        //std::fs::write("wrs_beat.txt", data).expect("Unable to write file");
-    }
-    println!("Script is finished running. Data saved in wrs_beat.txt. (jk)");
+    let mut f = std::fs::File::create("wrs_beat.txt").expect("Could not create file: wrs_beat.txt");
+    f.write_all(&data.into_bytes()).expect("Could not write to file: wrs_beat.txt");
+    //std::fs::write("wrs_beat.txt", data).expect("Unable to write file");
+
+    println!("Script is finished running. Data saved in wrs_beat.txt.");
 }
